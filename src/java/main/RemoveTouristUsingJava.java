ed569097b7e6d6944ca15dd3bec93ae5eb85a66a
zqFan <zeno_van@163.com>
2020-04-16 06:11:37 +0800

    code format

--- src/java/main/RemoveTouristUsingJava.java
+++ src/java/main/RemoveTouristUsingJava.java
@@ -3,121 +3,105 @@ package main;
 import java.util.*;
 import java.io.*;
 
-public class RemoveTouristUsingJava
-{
-	   public static void main(String[] args)
-	   {         
-		      final String FORMAT      = "P3";
-			  
-			  System.out.println();
-			  for (int i = 0; i < args.length; i++)
-	          {		            
-					 if (args[i].startsWith("filtered"))
-					 {
-						  System.out.println("It is better to delete all the previous versions of the filtered ppm files " );
-						  System.out.println("before running this program" );
-						  System.out.println();
-						  return;
-				     }	 
-	          }
+public class RemoveTouristUsingJava {
+    public static void main(String[] args) {
+        final String FORMAT = "P3";
 
-              int numOfImageFiles = args.length;
-			 
-	          if( numOfImageFiles < 3 )
-	          {
-		          System.out.println("\nYou need more images files");	
-				  return;
-	          }
+        System.out.println();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("filtered")) {
+                System.out.println("It is better to delete all the previous versions of the filtered ppm files ");
+                System.out.println("before running this program");
+                System.out.println();
+                return;
+            }
+        }
 
-              ArrayList<ImagePPM> images = new ArrayList<ImagePPM>();
+        int numOfImageFiles = args.length;
 
-			  for (int i=0; i < numOfImageFiles; i++)
-			  {
-					String imageName = args[i];
-										                
-                    ImagePPM pic = new ImagePPM(imageName);
+        if (numOfImageFiles < 3) {
+            System.out.println("\nYou need more images files");
+            return;
+        }
 
-					pic.getImageData();
-				    
-					images.add(pic);
-				}            
-			   //###################################################################
-               ArrayList<Integer> filteredImage = new ArrayList<Integer>();
+        ArrayList<ImagePPM> images = new ArrayList<ImagePPM>();
 
-	           filteredImage.add( images.get(0).getWidth()      );
-	           filteredImage.add( images.get(0).getHeight()     );
-          	   filteredImage.add( images.get(0).getMaxColour()  );
+        for (int i = 0; i < numOfImageFiles; i++) {
+            String imageName = args[i];
 
-	           for(int i=0; i < images.get(0).getSizeOfImage(); i++)
-	           {  
-				   ArrayList<Integer> tempArray = new ArrayList<Integer>();
-       
- 				   for(int j=0; j < numOfImageFiles; j++)
-		           {			            						  
-						 tempArray.add( images.get(j).getAsciiValueAtIndex(i) );  
-		           }
+            ImagePPM pic = new ImagePPM(imageName);
+
+            pic.getImageData();
+
+            images.add(pic);
+        }
+        //###################################################################
+        ArrayList<Integer> filteredImage = new ArrayList<Integer>();
+
+        filteredImage.add(images.get(0).getWidth());
+        filteredImage.add(images.get(0).getHeight());
+        filteredImage.add(images.get(0).getMaxColour());
+
+        for (int i = 0; i < images.get(0).getSizeOfImage(); i++) {
+            ArrayList<Integer> tempArray = new ArrayList<Integer>();
+
+            for (int j = 0; j < numOfImageFiles; j++) {
+                tempArray.add(images.get(j).getAsciiValueAtIndex(i));
+            }
 	             					
 				   /* System.out.println("Before the sort ------------------------------" );	
 					   for(int p=0; p < mode.size(); p++)
 	                   {   System.out.println("mode.get("+p+") is "+mode.get(p)+" " ); } 
 				   */
 
-				   Collections.sort(tempArray);
+            Collections.sort(tempArray);
 
                    /* System.out.println("After the sort ------------------------------" );	
 					  for(int p=0; p < mode.size(); p++)
 	                  {  System.out.println("mode.get("+p+") is "+mode.get(p)+" " ); }   
 				   */
-	               
-				   int medianValue = 0;
 
-				   if ( tempArray.size()/2 == 0)
-				   {
-					   int first_middle_index  =  ( tempArray.size() / 2 );
- 		               int first_middle_value  =  tempArray.get(first_middle_index);
- 
- 		               int second_middle_index =  first_middle_index + 1;
- 		               int second_middle_value =  tempArray.get(second_middle_index);   
-         
-                       medianValue = (first_middle_value + second_middle_value) / 2;
-				   }
-				   else
-				   {
-                       int midindex = (tempArray.size() + 1)/2;
-                       medianValue  = tempArray.get(midindex);
-				   }
-	
-				   filteredImage.add(medianValue);
-	           }
+            int medianValue = 0;
 
-               //##################################################################################
+            if (tempArray.size() / 2 == 0) {
+                int first_middle_index = (tempArray.size() / 2);
+                int first_middle_value = tempArray.get(first_middle_index);
 
-			   BufferedWriter writer = null;
-              
-			   try 
-			   {
-                    writer = new BufferedWriter(new FileWriter(new File("filteredImageUsingJava.ppm")));
-				    writer.write(FORMAT+"\n");
-					writer.write(filteredImage.get(0)+" "+filteredImage.get(1)+"\n");
-				    writer.write(filteredImage.get(2)+"\n");
+                int second_middle_index = first_middle_index + 1;
+                int second_middle_value = tempArray.get(second_middle_index);
 
-					for(int i=3; i < filteredImage.size(); i++)
-	                {
-						   // format file nicely
-  		                   if(i % 3 == 0 && i != 3)
-		                   {
-                   	 	  	   writer.write("\n");
-		                   }
-  		                   writer.write(filteredImage.get(i)+" ");		           										       
-  	                }
-				    writer.flush();
-				    writer.close();
-                } 			                                 
-			    catch (Exception e) 	
-			    {
-                    e.printStackTrace();
-                }         
-	   }
+                medianValue = (first_middle_value + second_middle_value) / 2;
+            } else {
+                int midindex = (tempArray.size() + 1) / 2;
+                medianValue = tempArray.get(midindex);
+            }
+
+            filteredImage.add(medianValue);
+        }
+
+        //##################################################################################
+
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(new File("filteredImageUsingJava.ppm")));
+            writer.write(FORMAT + "\n");
+            writer.write(filteredImage.get(0) + " " + filteredImage.get(1) + "\n");
+            writer.write(filteredImage.get(2) + "\n");
+
+            for (int i = 3; i < filteredImage.size(); i++) {
+                // format file nicely
+                if (i % 3 == 0 && i != 3) {
+                    writer.write("\n");
+                }
+                writer.write(filteredImage.get(i) + " ");
+            }
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 /*
