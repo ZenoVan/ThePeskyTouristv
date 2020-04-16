a179cf1359aae9d93440bb2a4f5b5d0b7b3b3d91
fanzeqiu <51195100007@stu.ecnu.edu.cn>
2020-04-16 10:01:41 +0800

    添加注释

--- src/java/main/ImagePPM.java
+++ src/java/main/ImagePPM.java
@@ -3,23 +3,55 @@ package main;
 import java.util.*;
 import java.io.*;
 
-
+/**
+ * ppm文件可以理解为一个矩阵
+ * 矩阵中的每一个元素都是 (R, G, B) 这样的一个结构体 （可以理解为一个向量，其中R G B均为正整数）
+ * RGB是一种表示颜色的方式 （R，G，B）分别代表红绿蓝，通过三原色的组合我们可以得到几乎所有颜色。数值越大表示加入的这种颜色越多。
+ * 比如 (255, 0, 0) 是大红色；(0, 0, 0)是白色；(255，255，255)是黑色
+ *
+ * 举个例子
+ * 如果文件形式为
+ *
+ * P3
+ * 2 3
+ * 255
+ * 0 0 0
+ * 255 255 255
+ * 1 2 3
+ * 4 5 6
+ * 7 8 9
+ * 10 11 12
+ *
+ * 第一行P3是编码方式，不用管
+ * 第二行表示 width=2 height=3 即这是一个2*3的矩阵，那么显然 size 的值应该就是 2*3=6
+ * 第三行表示 RGB 的取值最大为255，即 R=255 时表示红色给满
+ * 因此下面还有6行数据，就是指的矩阵中的6个元素
+ * 第一个点表示白色，第二个是黑色……
+ *
+ * 图片其实就是由非常多个带颜色的点组成的，因此只要点够多，比如一个 495 * 557 的矩阵，就可以表示一小张图片啦
+ *
+ */
 public class ImagePPM {
-    private String format;
-    private int width;
-    private int height;
-    private int maxColour;
+    private String format; // 编码方式
+    private int width; // 矩阵的宽
+    private int height; // 矩阵的高
+    private int maxColour; // 矩阵中元素里的三个数值的取值上限，即 R G B 的取值范围为[0, maxColour]
 
-    private int size;
+    private int size; // 矩阵中一共有多少个元素
     private File file;
 
-    private ArrayList<Integer> ascii;
+    private ArrayList<Integer> ascii; // ascii这个变量就是把这个 n行m列 的矩阵 换成 1行n*m列 的向量
 
     public ImagePPM(File file) {
         this.file = file;
         ascii = new ArrayList<Integer>();
     }
 
+    /**
+     * 初始化ppm文件
+     * 并检测该文件是否复合ppm文件的格式
+     * 不必细看
+     */
     public void getImageData() {
         System.out.println("fileName is " + file.getName());
         try {
--- src/java/main/RemoveTouristUsingJava.java
+++ src/java/main/RemoveTouristUsingJava.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class RemoveTouristUsingJava {
 
     public static void main(String[] args) {
-        final String FORMAT = "P3";
+        final String FORMAT = "P3"; // 编码方式，不用管
         /*
         System.out.println();
         for (int i = 0; i < args.length; i++) {
@@ -18,9 +18,10 @@ public class RemoveTouristUsingJava {
             }
         }
         */
-        File ppmDir = new File("src/resources/ppm");
-        File resDir = new File("src/resources/result");
+        File ppmDir = new File("src/resources/ppm"); // 从这个路径中获取有游客的照片
+        File resDir = new File("src/resources/result"); // 在这个路径下生成最终结果
 
+        // 这一段是在检验获取的照片是不是ppm格式，如果不是的话就不要
         File[] ppmImages = ppmDir.listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
@@ -29,15 +30,17 @@ public class RemoveTouristUsingJava {
             }
         });
 
-        int numOfImageFiles = ppmImages.length;
+        int numOfImageFiles = ppmImages.length; // 一共有numOfImageFiles张图片
 
+        // 如果初始图片太少，那就直接退出程序，表示需要更多图片
         if (numOfImageFiles < 3) {
             System.out.println("\nYou need more images files");
             return;
         }
 
-        ArrayList<ImagePPM> images = new ArrayList<ImagePPM>();
+        ArrayList<ImagePPM> images = new ArrayList<ImagePPM>(); // 注意 变量images 表示所有的 有游客的照片 形成的数组
 
+        // 把所有 有游客照片 的文件转化为 ImagePPM 这个类的对象，然后放到变量images里去。一个 Data to Object 的操作，看不懂没事
         for (int i = 0; i < numOfImageFiles; i++) {
 //            String imageName = ppmImages[i].getName();
 
@@ -47,34 +50,29 @@ public class RemoveTouristUsingJava {
 
             images.add(pic);
         }
+
+        // 完成了 有游客照片 的输入工作
         //###################################################################
+        // 下面开始转换成 无游客照片
+
         ArrayList<Integer> filteredImage = new ArrayList<Integer>();
 
         filteredImage.add(images.get(0).getWidth());
         filteredImage.add(images.get(0).getHeight());
         filteredImage.add(images.get(0).getMaxColour());
 
-        for (int i = 0; i < images.get(0).getSizeOfImage(); i++) {
+        for (int i = 0; i < images.get(0).getSizeOfImage(); i++) { // 对第一张图片的所有点做遍历操作，每次取一个点
             ArrayList<Integer> tempArray = new ArrayList<Integer>();
 
-            for (int j = 0; j < numOfImageFiles; j++) {
+            for (int j = 0; j < numOfImageFiles; j++) { //
                 tempArray.add(images.get(j).getAsciiValueAtIndex(i));
             }
-	             					
-				   /* System.out.println("Before the sort ------------------------------" );	
-					   for(int p=0; p < mode.size(); p++)
-	                   {   System.out.println("mode.get("+p+") is "+mode.get(p)+" " ); } 
-				   */
 
-            Collections.sort(tempArray);
 
-                   /* System.out.println("After the sort ------------------------------" );	
-					  for(int p=0; p < mode.size(); p++)
-	                  {  System.out.println("mode.get("+p+") is "+mode.get(p)+" " ); }   
-				   */
+            Collections.sort(tempArray); // 排序
 
+            // 取中位数
             int medianValue = 0;
-
             if (tempArray.size() / 2 == 0) {
                 int first_middle_index = (tempArray.size() / 2);
                 int first_middle_value = tempArray.get(first_middle_index);
@@ -94,7 +92,7 @@ public class RemoveTouristUsingJava {
         //##################################################################################
 
         BufferedWriter writer;
-
+        // 把得到的中位数写进去
         try {
             writer = new BufferedWriter(new FileWriter(new File(resDir + "/result.ppm")));
             writer.write(FORMAT + "\n");
