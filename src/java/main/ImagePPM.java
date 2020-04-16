d7a1db90dd8fc32daaad49121ecd6806a6dd03be
fanzeqiu <51195100007@stu.ecnu.edu.cn>
2020-04-16 09:08:11 +0800

    删除生成的结果文件 为ImagePPM code format

--- src/java/main/ImagePPM.java
+++ src/java/main/ImagePPM.java
@@ -4,42 +4,37 @@ import java.util.*;
 import java.io.*;
 
 
-public class ImagePPM
-{
-	   private String  format;
-       private int     width;
-       private int     height;
-	   private int     maxColour;
+public class ImagePPM {
+    private String format;
+    private int width;
+    private int height;
+    private int maxColour;
 
-       private int     size;
-	   private File  file;
-	
-	   private ArrayList<Integer> ascii;
+    private int size;
+    private File file;
 
-       public ImagePPM(File file)
-	   {
-	          this.file = file;
-			  ascii    = new ArrayList<Integer>();
-	   }
+    private ArrayList<Integer> ascii;
 
-	   public void getImageData()
- 	   {
-               System.out.println("fileName is "+file.getName());
-			   try 
-			   {
-					  Scanner sc  =  new Scanner(file);
+    public ImagePPM(File file) {
+        this.file = file;
+        ascii = new ArrayList<Integer>();
+    }
 
-					  format      =  sc.next();
-                      
-					  if( format.equals("P3") )
-		              {
-						  width       = sc.nextInt();
-						  height      = sc.nextInt();
-						  maxColour   = sc.nextInt();	
-						  
-                          size        = (width * height) * 3;    // size = (495 * 557) * 3;  		                                                   
-										                         // size = (275715)    * 3;
-										                         // size =  827145
+    public void getImageData() {
+        System.out.println("fileName is " + file.getName());
+        try {
+            Scanner sc = new Scanner(file);
+
+            format = sc.next();
+
+            if (format.equals("P3")) {
+                width = sc.nextInt();
+                height = sc.nextInt();
+                maxColour = sc.nextInt();
+
+                size = (width * height) * 3;    // size = (495 * 557) * 3;
+                // size = (275715)    * 3;
+                // size =  827145
 
 	                      /*  System.out.println("format    is "+format);
 						      System.out.println("width     is "+width);
@@ -48,62 +43,52 @@ public class ImagePPM
 		                      System.out.println("size      is "+size);
 						  */
 
-						  int temp;
+                int temp;
 
-                          while ( sc.hasNextInt() )  
-			              {
-                              temp = sc.nextInt();
-					          ascii.add(temp);
-                          }						 	
-						  // pixel = R G B
-						  // line number        data
-						  //     1              P3
-						  //     2              495 557
-						  //     3              255
-						  //     4              240 244 253
-						  //     .              ...........
-						  //     .              ...........
-						  //    275716          174 165 132
-						  //    275717          168 159 126
-						  //    275718          164 155 122
-						  //    275719          no pixel values ie blank
-                      }
-					  else
-				      {
-					     System.out.println("The image is not correct format.");
-					  }
-					  sc.close(); 
-                } 
-                catch (FileNotFoundException e) 
-			    {
-                      e.printStackTrace();
-				      System.out.println("Input file opening failed");
-                }	   
-	   }
+                while (sc.hasNextInt()) {
+                    temp = sc.nextInt();
+                    ascii.add(temp);
+                }
+                // pixel = R G B
+                // line number        data
+                //     1              P3
+                //     2              495 557
+                //     3              255
+                //     4              240 244 253
+                //     .              ...........
+                //     .              ...........
+                //    275716          174 165 132
+                //    275717          168 159 126
+                //    275718          164 155 122
+                //    275719          no pixel values ie blank
+            } else {
+                System.out.println("The image is not correct format.");
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Input file opening failed");
+        }
+    }
 
-	   public int  getWidth()
-	   {
-	          return width;
-	   }
+    public int getWidth() {
+        return width;
+    }
 
-	   public int  getHeight()
-	   {
-	          return height;
-	   }
+    public int getHeight() {
+        return height;
+    }
 
-	   public int  getMaxColour()
-	   {
-              return maxColour;
-	   }
+    public int getMaxColour() {
+        return maxColour;
+    }
 
-	   public int  getSizeOfImage()
-	   {
-	          return size;
-	   }
-	   
-	   public int  getAsciiValueAtIndex(int position)
-	   {
-	          return ascii.get(position);
-	   }
-	  
+    public int getSizeOfImage() {
+        return size;
+    }
+
+    public int getAsciiValueAtIndex(int position) {
+        return ascii.get(position);
+    }
+
 }
